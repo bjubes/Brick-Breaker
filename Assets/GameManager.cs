ad3a247899d25858e73f9fb08b7aabdc87c09f6b
iD Student <iDStudent@STRATTON-SIX-SIXTY.MIT.EDU>
2016-07-21 18:15:32 -0400

    initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject ball;
+	public GameObject block;
+
+	int score;
+
+	float leftMostBlockX = -2.3f;
+	float blockHeight = 0.4f;
+
+	void Start () {
+		SpawnBlockRow ();
+	}
+	
+	void Update () {
+		
+	}
+
+
+	void SpawnBlockRow() {
+		float screenHeight = Camera.main.orthographicSize;
+		float spawnHeight = screenHeight - blockHeight;
+
+		int amountToSpawn = Random.Range (1, 6);
+		Debug.Log (amountToSpawn);
+
+		var SpawnLocations = new List<float>();
+
+		int i = 0;
+		while ( i < amountToSpawn) { //loop until you hit the spawn ammount
+			int location = Random.Range(0,5); //TODO: make it spawn 6 and make six fit perfectly in the screen
+			if (!SpawnLocations.Contains (location)) {
+				SpawnLocations.Add (location);
+				i+=1;
+			}
+		}
+		foreach (float loc in SpawnLocations){
+			var xVal = loc + leftMostBlockX;
+		
+
+			Vector2 pos = new Vector2 (xVal, spawnHeight);
+			var Block = (GameObject)Instantiate(block, pos, Quaternion.identity);
+		}
+	}
+
+}
---
+++ Assets/moveBall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveBall : MonoBehaviour {
+
+	float speed = 3f;
+	bool clicked = false;
+
+	Rigidbody2D rb;
+
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D> ();
+		if (rb == null) {
+			Debug.Log ("no rigid body");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		clicked = Input.GetMouseButtonDown(0);
+	}
+
+	void FixedUpdate() {
+		if (clicked) {
+			rb.velocity = Vector2.up * speed;
+		}
+
+		rb.velocity = rb.velocity.normalized * speed;
+	}
+}
